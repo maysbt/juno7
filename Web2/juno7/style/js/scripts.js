81cd5d6d6d6f14976f0e40cd12e06958b2b0539d
Brandon Mays <116676067+maysbt@users.noreply.github.com>
2022-10-27 20:50:08 -0400

    Add files via upload

---
+++ Web2/juno7/style/js/scripts.js
@@ -0,0 +1,196 @@
+$(document).ready(function() {
+    'use strict';
+    /*-----------------------------------------------------------------------------------*/
+    /*	STICKY HEADER
+    /*-----------------------------------------------------------------------------------*/
+    var options = {
+        offset: 350,
+        offsetSide: 'top',
+        classes: {
+            clone: 'banner--clone fixed',
+            stick: 'banner--stick',
+            unstick: 'banner--unstick'
+        },
+        onStick: function() {
+            $($.SmartMenus.Bootstrap.init);
+        }
+    };
+    var banner = new Headhesive('.navbar', options);
+    /*-----------------------------------------------------------------------------------*/
+    /*	HAMBURGER MENU ICON
+    /*-----------------------------------------------------------------------------------*/
+    $(".nav-bars").on( "click", function() {
+        $(".nav-bars").toggleClass("is-active");
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	FOTORAMA
+	/*-----------------------------------------------------------------------------------*/
+    $('.fotorama').fotorama({
+        spinner: {
+            color: 'rgba(0, 0, 0, 0)'
+        }
+    });
+    $('.fotorama-item').append(' ');
+    /*-----------------------------------------------------------------------------------*/
+    /*	IMAGE ICON HOVER
+    /*-----------------------------------------------------------------------------------*/
+    $('.overlay a').prepend('<span class="over"><span></span></span>');
+    /*-----------------------------------------------------------------------------------*/
+    /*	PARALLAX MOBILE
+    /*-----------------------------------------------------------------------------------*/
+    if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i)) {
+        $('.parallax').addClass('mobile');
+    }
+    /*-----------------------------------------------------------------------------------*/
+    /*	SWIPER
+    /*-----------------------------------------------------------------------------------*/
+    $('.swiper-container.image-blog-wide').each(function() {
+        $(this).swiper({
+            pagination: '.image-blog-wide-wrapper .swiper-pagination',
+            nextButton: '.image-blog-wide-wrapper .swiper-button-next',
+            prevButton: '.image-blog-wide-wrapper .swiper-button-prev',
+            slidesPerView: 3,
+            breakpoints: {
+                991: {
+                    slidesPerView: 1
+                },
+                1681: {
+                    slidesPerView: 2
+                }
+            },
+            centeredSlides: false,
+            paginationClickable: true,
+            spaceBetween: 10,
+            grabCursor: true
+        });
+        var $swipers = $(this);
+    });
+    $('.swiper-container.image-blog').each(function() {
+        $(this).swiper({
+            pagination: '.image-blog-wrapper .swiper-pagination',
+            nextButton: '.image-blog-wrapper .swiper-button-next',
+            prevButton: '.image-blog-wrapper .swiper-button-prev',
+            slidesPerView: 2,
+            breakpoints: {
+                991: {
+                    slidesPerView: 1
+                },
+                1681: {
+                    slidesPerView: 2
+                }
+            },
+            centeredSlides: false,
+            paginationClickable: true,
+            spaceBetween: 10,
+            grabCursor: true
+        });
+        var $swipers = $(this);
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	LIGHTGALLERY
+	/*-----------------------------------------------------------------------------------*/
+    $('.light-gallery').lightGallery({
+        thumbnail: false,
+        selector: '.lgitem',
+        animateThumb: true,
+        showThumbByDefault: false,
+        download: false,
+        autoplayControls: false,
+        zoom: false,
+	    fullScreen: false,
+        thumbWidth: 100,
+        thumbContHeight: 80,
+        hash: false,
+        videoMaxWidth: '1000px'
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	TAB COLLAPSE
+    /*-----------------------------------------------------------------------------------*/
+    $('#tab1').tabCollapse({
+        tabsClass: 'hidden-sm hidden-xs',
+        accordionClass: 'visible-sm visible-xs'
+    });
+    $('#tab1').on('shown-accordion.bs.tabcollapse', function() {
+        $('.panel-group').find('.panel-default:has(".in")').addClass('panel-active');
+        $('.panel-group').on('shown.bs.collapse', function(e) {
+            $(e.target).closest('.panel-default').addClass(' panel-active');
+        }).on('hidden.bs.collapse', function(e) {
+            $(e.target).closest('.panel-default').removeClass(' panel-active');
+        });
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	TOGGLE
+    /*-----------------------------------------------------------------------------------*/
+    $('.panel-group').find('.panel-default:has(".in")').addClass('panel-active');
+    $('.panel-group').on('shown.bs.collapse', function(e) {
+        $(e.target).closest('.panel-default').addClass(' panel-active');
+    }).on('hidden.bs.collapse', function(e) {
+        $(e.target).closest('.panel-default').removeClass(' panel-active');
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	PROGRESS BAR
+    /*-----------------------------------------------------------------------------------*/
+    $('.progress-list .progress .bar').progressBar({
+        shadow: false,
+        percentage: false,
+        animation: true,
+        height: 12
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	DATA REL
+    /*-----------------------------------------------------------------------------------*/
+    $('a[data-rel]').each(function() {
+        $(this).attr('rel', $(this).data('rel'));
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	TOOLTIP
+    /*-----------------------------------------------------------------------------------*/
+    if ($("[rel=tooltip]").length) {
+        $("[rel=tooltip]").tooltip();
+    }
+    /*-----------------------------------------------------------------------------------*/
+    /*	PRETTIFY
+    /*-----------------------------------------------------------------------------------*/
+    window.prettyPrint && prettyPrint();
+    /*-----------------------------------------------------------------------------------*/
+    /*	VANILLA
+    /*-----------------------------------------------------------------------------------*/
+    var myForm;
+    myForm = new VanillaForm($("form.vanilla-form"));
+    /*-----------------------------------------------------------------------------------*/
+    /*	COUNTER UP
+    /*-----------------------------------------------------------------------------------*/
+    $('.fcounter').counterUp({
+        delay: 50,
+        time: 1000
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	COLLAGEPLUS
+    /*-----------------------------------------------------------------------------------*/
+    collage();
+    function collage() {
+        $('#collage-large').removeWhitespace().collagePlus({
+            'fadeSpeed': 5000,
+            'targetHeight': 400,
+            'effect': 'effect-2',
+            'direction': 'vertical',
+            'allowPartialLastRow': true
+        });
+        $('#collage-medium').removeWhitespace().collagePlus({
+            'fadeSpeed': 5000,
+            'targetHeight': 300,
+            'effect': 'effect-2',
+            'direction': 'vertical',
+            'allowPartialLastRow': true
+        });
+    };
+    // This is just for the case that the browser window is resized
+    var resizeTimer = null;
+    $(window).on('resize', function() {
+        // hide all the images until we resize them
+        $('.collage .collage-image-wrapper').css("opacity", 0);
+        // set a timer to re-apply the plugin
+        if (resizeTimer) clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(collage, 200);
+    });
+});
